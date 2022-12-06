92927ae1a5631acb531571faa0b109c18fd5dd59
rod-sousa <rod.sousa899@gmail.com>
2022-12-06 19:13:05 -0300

    Implemented imc calculation and corresponding metric in the layout

--- app/src/main/java/rodsousa/dev/br/fitnessapp/ui/presentation/fragment/ImcFragment.kt
+++ app/src/main/java/rodsousa/dev/br/fitnessapp/ui/presentation/fragment/ImcFragment.kt
@@ -1,9 +1,13 @@
 package rodsousa.dev.br.fitnessapp.ui.presentation.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import rodsousa.dev.br.fitnessapp.R
@@ -14,6 +18,7 @@ import rodsousa.dev.br.fitnessapp.data.model.SexSelected.MEN
 import rodsousa.dev.br.fitnessapp.data.model.SexSelected.WOMAN
 import rodsousa.dev.br.fitnessapp.databinding.FragmentImcBinding
 import rodsousa.dev.br.fitnessapp.ui.viewmodel.FitnessViewModel
+
 
 class ImcFragment : Fragment() {
 
@@ -36,25 +41,46 @@ class ImcFragment : Fragment() {
 
         selectedSex()
 
-//        binding.btnConfirmed.setOnClickListener{
-//
-//            val weight = binding.edtWeight.text.toString()
-//            val height = binding.edtHeight.text.toString()
-//
-//            val weightDouble = if(weight.isBlank()) 0.0 else weight.toDouble()
-//            val heightDouble = if(height.isBlank()) 0.0 else height.toDouble()
-//
-//            if(!validate(weightDouble, heightDouble)){
-//                Toast.makeText(context, R.string.fields_message, Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//
+        binding.edtHeight.addTextChangedListener {
+            calculateImcAndPositionIndicatorLayout()
+        }
+
+        binding.edtWeight.addTextChangedListener {
+            calculateImcAndPositionIndicatorLayout()
+        }
+
+        binding.edtAge.addTextChangedListener {
+            calculateImcAndPositionIndicatorLayout()
+        }
+
 //            val result = calculateImc(weightDouble, heightDouble)
 //
 //            alertDialogMessage(result)
 //
-//            hideKeyboard()
+
 //        }
+    }
+
+    private fun calculateImcAndPositionIndicatorLayout() {
+        val weight = binding.edtWeight.text.toString()
+        val height = binding.edtHeight.text.toString()
+        val age = binding.edtAge.text.toString()
+
+        if (validate(weight, height, age)) {
+            return
+        }
+        val weightDouble = if (weight.isBlank()) 0.0 else weight.toDouble()
+        val heightDouble = if (height.isBlank()) 0.0 else height.toDouble()
+        val ageInt = if (age.isBlank()) 0 else height.toInt()
+
+        val result = calculateImc(weightDouble, heightDouble)
+
+        discoverCorrespondingImcFootage(result)
+
+        val resultFormated = String.format("%.1f", result)
+        binding.tvResult.text = resultFormated
+
+        //            hideKeyboard()
     }
 
     private fun selectedSex() {
@@ -81,12 +107,37 @@ class ImcFragment : Fragment() {
         }
     }
 
-//    private fun hideKeyboard() {
-//        val service = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        service.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
-//    }
-//
-//    private fun alertDialogMessage(result: Double) {
+    private fun discoverCorrespondingImcFootage(resultCalcImc: Double) {
+        val resultCalcBiasPercent = when {
+            resultCalcImc < 15.0 -> 0.05f
+            resultCalcImc < 16.0 -> 0.095f
+            resultCalcImc < 18.5 -> 0.14f
+            resultCalcImc < 20.0 -> 0.25f
+            resultCalcImc < 22.5 -> 0.29f
+            resultCalcImc < 25.0 -> 0.35f
+            resultCalcImc < 27.5 -> 0.46f
+            resultCalcImc < 30.0 -> 0.54f
+            resultCalcImc < 35.0 -> 0.68f
+            resultCalcImc < 40.0 -> 0.75f
+            resultCalcImc < 45.0 -> 0.87f
+            resultCalcImc > 45.0 -> 0.95f
+            else -> 0.0f
+        }
+        setImcChartLayout(resultCalcBiasPercent)
+    }
+
+    private fun setImcChartLayout(bias: Float) {
+        val params = binding.ivMeter.layoutParams as ConstraintLayout.LayoutParams
+        params.horizontalBias = bias
+        binding.ivMeter.layoutParams = params
+    }
+
+    private fun hideKeyboard() {
+        val service = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        service.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
+    }
+
+    //    private fun alertDialogMessage(result: Double) {
 //        AlertDialog.Builder(context)
 //            .setTitle(getString(R.string.imc_response, result))
 //            .setMessage(imcResponse(result))
@@ -111,9 +162,9 @@ class ImcFragment : Fragment() {
 //        }
 //    }
 //
-//    private fun calculateImc(weight: Double, height: Double) =
-//        weight / ( (height/100) * (height/100) )
-//
-//    private fun validate(weight: Double, height: Double) =
-//        weight != 0.0 && height != 0.0
+    private fun calculateImc(weight: Double, height: Double) =
+        weight / ( (height/100) * (height/100) )
+
+    private fun validate(weight: String, height: String, age: String) =
+        weight.isBlank() || height.isBlank() || age.isBlank()
 }
