0d879e0460978a053247a39762f9420beee30fe4
rod-sousa <rod.sousa899@gmail.com>
2022-12-05 21:24:29 -0300

    Created to Object SexSelected and implementation to progress stat

---
+++ app/src/main/java/rodsousa/dev/br/fitnessapp/data/model/SexSelected.kt
@@ -0,0 +1,28 @@
+package rodsousa.dev.br.fitnessapp.data.model
+
+import androidx.annotation.IntegerRes
+import rodsousa.dev.br.fitnessapp.R
+
+object SexSelected {
+    const val IC_MEN = R.drawable.ic_men
+    const val IC_MEN_SELETCED = R.drawable.ic_men_selected
+    const val IC_WOMAN = R.drawable.ic_woman
+    const val IC_WOMAN_SELECTED = R.drawable.ic_woman_selected
+
+    const val WOMAN = "WOMAN_SELECTED"
+    const val MEN = "MEN_SELECTED"
+
+    private var selected : String = ""
+
+    @IntegerRes
+    fun selectedSex(sex : String) : Int?{
+        selected = sex
+        return when(sex){
+            WOMAN -> IC_WOMAN_SELECTED
+            MEN   -> IC_MEN_SELETCED
+            else  -> null
+        }
+    }
+
+    fun getSelected() = selected
+}
--- app/src/main/java/rodsousa/dev/br/fitnessapp/ui/presentation/fragment/ImcFragment.kt
+++ app/src/main/java/rodsousa/dev/br/fitnessapp/ui/presentation/fragment/ImcFragment.kt
@@ -1,17 +1,17 @@
 package rodsousa.dev.br.fitnessapp.ui.presentation.fragment
 
-import android.app.AlertDialog
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import rodsousa.dev.br.fitnessapp.R
+import rodsousa.dev.br.fitnessapp.data.model.SexSelected
+import rodsousa.dev.br.fitnessapp.data.model.SexSelected.IC_MEN
+import rodsousa.dev.br.fitnessapp.data.model.SexSelected.IC_WOMAN
+import rodsousa.dev.br.fitnessapp.data.model.SexSelected.MEN
+import rodsousa.dev.br.fitnessapp.data.model.SexSelected.WOMAN
 import rodsousa.dev.br.fitnessapp.databinding.FragmentImcBinding
 import rodsousa.dev.br.fitnessapp.ui.viewmodel.FitnessViewModel
 
@@ -32,60 +32,88 @@ class ImcFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnConfirmed.setOnClickListener{
+        binding.myToolBar.inflateMenu(R.menu.appbar_imc)
 
-            val weight = binding.edtWeight.text.toString()
-            val height = binding.edtHeight.text.toString()
+        selectedSex()
 
-            val weightDouble = if(weight.isBlank()) 0.0 else weight.toDouble()
-            val heightDouble = if(height.isBlank()) 0.0 else height.toDouble()
+//        binding.btnConfirmed.setOnClickListener{
+//
+//            val weight = binding.edtWeight.text.toString()
+//            val height = binding.edtHeight.text.toString()
+//
+//            val weightDouble = if(weight.isBlank()) 0.0 else weight.toDouble()
+//            val heightDouble = if(height.isBlank()) 0.0 else height.toDouble()
+//
+//            if(!validate(weightDouble, heightDouble)){
+//                Toast.makeText(context, R.string.fields_message, Toast.LENGTH_SHORT).show()
+//                return@setOnClickListener
+//            }
+//
+//            val result = calculateImc(weightDouble, heightDouble)
+//
+//            alertDialogMessage(result)
+//
+//            hideKeyboard()
+//        }
+    }
 
-            if(!validate(weightDouble, heightDouble)){
-                Toast.makeText(context, R.string.fields_message, Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+    private fun selectedSex() {
+        binding.ivMen.setOnClickListener {
+            val selected = SexSelected.getSelected()
+            if (selected.isNotBlank()) {
+                when (selected) {
+                    MEN -> return@setOnClickListener
+                    WOMAN -> binding.ivWoman.setImageResource(IC_WOMAN)
+                }
             }
+            binding.ivMen.setImageResource(SexSelected.selectedSex(MEN)!!)
+        }
 
-            val result = calculateImc(weightDouble, heightDouble)
-
-            alertDialogMessage(result)
-
-            hideKeyboard()
+        binding.ivWoman.setOnClickListener {
+            val selected = SexSelected.getSelected()
+            if (selected.isNotBlank()) {
+                when (selected) {
+                    WOMAN -> return@setOnClickListener
+                    MEN -> binding.ivMen.setImageResource(IC_MEN)
+                }
+            }
+            binding.ivWoman.setImageResource(SexSelected.selectedSex(WOMAN)!!)
         }
     }
 
-    private fun hideKeyboard() {
-        val service = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        service.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
-    }
-
-    private fun alertDialogMessage(result: Double) {
-        AlertDialog.Builder(context)
-            .setTitle(getString(R.string.imc_response, result))
-            .setMessage(imcResponse(result))
-            .setPositiveButton(android.R.string.ok) { dialog, witch ->
-
-
-            }
-            .create().show()
-    }
-
-    @StringRes
-    private fun imcResponse(resultCalcImc: Double):Int{
-        return when {
-            resultCalcImc < 15.0 -> R.string.imc_severely_low_weight
-            resultCalcImc < 16.0 -> R.string.imc_very_low_weight
-            resultCalcImc < 18.5 -> R.string.imc_low_weight
-            resultCalcImc < 25.0 -> R.string.normal
-            resultCalcImc < 30.0 -> R.string.imc_high_weight
-            resultCalcImc < 35.0 -> R.string.imc_so_high_weight
-            resultCalcImc < 40.0 -> R.string.imc_severely_high_weight
-            else -> R.string.imc_extreme_weight
-        }
-    }
-
-    private fun calculateImc(weight: Double, height: Double) =
-        weight / ( (height/100) * (height/100) )
-
-    private fun validate(weight: Double, height: Double) =
-        weight != 0.0 && height != 0.0
+//    private fun hideKeyboard() {
+//        val service = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        service.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
+//    }
+//
+//    private fun alertDialogMessage(result: Double) {
+//        AlertDialog.Builder(context)
+//            .setTitle(getString(R.string.imc_response, result))
+//            .setMessage(imcResponse(result))
+//            .setPositiveButton(android.R.string.ok) { dialog, witch ->
+//
+//
+//            }
+//            .create().show()
+//    }
+//
+//    @StringRes
+//    private fun imcResponse(resultCalcImc: Double):Int{
+//        return when {
+//            resultCalcImc < 15.0 -> R.string.imc_severely_low_weight
+//            resultCalcImc < 16.0 -> R.string.imc_very_low_weight
+//            resultCalcImc < 18.5 -> R.string.imc_low_weight
+//            resultCalcImc < 25.0 -> R.string.normal
+//            resultCalcImc < 30.0 -> R.string.imc_high_weight
+//            resultCalcImc < 35.0 -> R.string.imc_so_high_weight
+//            resultCalcImc < 40.0 -> R.string.imc_severely_high_weight
+//            else -> R.string.imc_extreme_weight
+//        }
+//    }
+//
+//    private fun calculateImc(weight: Double, height: Double) =
+//        weight / ( (height/100) * (height/100) )
+//
+//    private fun validate(weight: Double, height: Double) =
+//        weight != 0.0 && height != 0.0
 }
